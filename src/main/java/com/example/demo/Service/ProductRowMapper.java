477c89106fa6655009e480c5a19721502424c83b
atishbarua <barua.ati@gmail.com>
2018-02-07 18:12:24 +0600

    AtishBarua

---
+++ src/main/java/com/example/demo/Service/ProductRowMapper.java
@@ -0,0 +1,10 @@
+package com.example.demo.Service;
+
+import com.example.demo.dto.Product;
+import org.springframework.jdbc.core.RowMapper;
+
+public class UserRowMapper implements RowMapper<Product>{
+
+    @Override
+    
+}
